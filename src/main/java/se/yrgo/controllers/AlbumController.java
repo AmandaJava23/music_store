11e416c76da89c9d8a0b77273f88e3a31df65df2
Amanda Vasilis <amavas0414@skola.goteborg.se>
2024-05-23 13:30:37 +0200

    random commit

--- src/main/java/se/yrgo/controllers/AlbumController.java
+++ src/main/java/se/yrgo/controllers/AlbumController.java
@@ -1,6 +1,46 @@
-package se.yrgo.controllers;
-
-public class AlbumController {
-
-
-}
+//package se.yrgo.controllers;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.servlet.ModelAndView;
+//import se.yrgo.data.AlbumRepository;
+//import se.yrgo.domain.Album;
+//
+//@Controller
+//@RequestMapping("/website/albums")
+//public class AlbumController {
+//
+//    @Autowired
+//    private AlbumRepository data;
+//
+//    //This method will save the album into the database
+//    @RequestMapping(value="/newAlbum.html",method= RequestMethod.POST)
+//    public String newAlbum(Album album){
+//
+//    }
+//
+//    //presenting the initial form to the user
+//    @RequestMapping(value="/newAlbum.html",method=RequestMethod.GET)
+//    public ModelAndView renderNewAlbumForm(){
+//
+//    }
+//
+//    //Responsible for listing all of the albums
+//    @RequestMapping(value="/list.html", method=RequestMethod.GET)
+//    public ModelAndView albums(){
+//        // TODO
+//    }
+//
+//    //Search for a album by its name
+//    @RequestMapping(value="/album/{title}")
+//    public ModelAndView showAlbumByName(@PathVariable("title") String title){
+//        // TODO
+//    }
+//}
+//
+//
+//
+//}
--- src/main/java/se/yrgo/domain/Album.java
+++ src/main/java/se/yrgo/domain/Album.java
@@ -1,8 +1,28 @@
 package se.yrgo.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Album {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String genre;
+    private Band band;
 
-    String name;
+    public Album() {
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
